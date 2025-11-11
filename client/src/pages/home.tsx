b4a7c7c044d389342a7d0b68bfae1f374ec312b1
Simon Senecky <sosenecky@gmail.com>
2025-11-11 19:59:35 +0100

    chore: push latest changes to main

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -360,21 +360,6 @@ export default function Home() {
           </motion.div>
         </div>
 
-        {/* Scroll indicator */}
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 1.5, duration: 1 }}
-          className="absolute bottom-8 left-1/2 -translate-x-1/2"
-        >
-          <motion.div
-            animate={{ y: [0, 10, 0] }}
-            transition={{ duration: 1.5, repeat: Infinity }}
-            className="w-6 h-10 border-2 border-gray-400 rounded-full p-1"
-          >
-            <motion.div className="w-1.5 h-1.5 bg-gray-600 rounded-full mx-auto" />
-          </motion.div>
-        </motion.div>
       </section>
 
       {/* About Section */}
